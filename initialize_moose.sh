bf8bf09c5c8dd03af505c538a0f18b1c659a5ee6
Jesse Carter <jesse.carter@gmail.com>
2020-08-06 12:18:07 -0600

    add script that pulls moose from local server and hacks local config to clone submodules from local server

---
+++ initialize_moose.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# this script loops over moose and its submodules and changes the *local* configuation of the submodule url's
+# without touching the files so moose can be used internally without changing any files
+#
+# with more thought I could come up with some slick recursive algorithm, but this works for now
+
+# ssh remote for internal git server
+REMOTE=git@hpcgitlab.hpc.inl.gov:`whoami`
+
+module load git
+
+# clone moose and initialize libmesh
+git clone $REMOTE/moose.git
+cd moose
+git submodule init libmesh  # initialize repo so it gets an entry in local config file (.git/config)
+git config --local submodule.libmesh.url $REMOTE/libmesh.git  # change libmesh url in local repo config
+git submodule update libmesh  # update uses the location in the config file
+
+# go into libmesh and initialize each of its submodules changing the local config like above
+cd libmesh
+git submodule init
+for sub in `git submodule status | cut -c2- | cut -d " " -f 2`  # loop over submodules of libmesh
+do
+  echo $sub
+  name=`basename $sub`
+  git config --local submodule.$sub.url $REMOTE/$name.git
+done
+git submodule update
+
+# go into each one of libmesh's submodules and initialize each one of those
+wd=$PWD
+for libmeshsub in `git submodule status | cut -c2- | cut -d " " -f 2`
+do
+  echo $libmeshsub
+  cd $libmeshsub
+  git submodule init
+  for sub in `git submodule status | cut -c2- | cut -d " " -f 2`
+  do
+    echo $sub
+    name=`basename $sub`
+    git config --local submodule.$sub.url $REMOTE/$name.git
+  done
+  git submodule update
+  cd $wd
+done
+
+# check if we're done
+cd ..
+git submodule update --init --recursive libmesh
+if [ $? -ne 0 ]
+then
+  echo "********************"
+  echo "something went wrong"
+  echo "********************"
+fi
